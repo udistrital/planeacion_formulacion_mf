cedf029c72142e69153229d0b1f11a840d4a0daf
C0C045 <55455142+C0C045@users.noreply.github.com>
2024-03-21 15:23:58 -0500

    feat: conexión con la tabla pendientes

--- src/app/services/verificarFormulario.ts
+++ src/app/services/verificarFormulario.ts
@@ -1,31 +1,24 @@
 import { Injectable } from '@angular/core'
-import { BehaviorSubject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class VerificarFormulario {
-  private formData = new BehaviorSubject<any[]>([]);
-  formData$ = this.formData.asObservable();
+  getCookie(name: string): string | undefined {
+    const value = '; ' + document.cookie;
+    const parts = value.split('; ' + name + '=');
 
-  // Estado para regresar a la lista de planes pendientes en seguimiento
-  private estadoLista = new BehaviorSubject<boolean>(false);
-  estadoLista$ = this.estadoLista.asObservable();
-
-  setFormData(data: any, vigencia: any, unidad: any) {
-    const currentFormData = this.formData.getValue();
-    currentFormData.push(data)
-    currentFormData.push(vigencia)
-    currentFormData.push(unidad)
-    this.formData.next(currentFormData);
+    if (parts.length == 2) {
+      return parts.pop()?.split(';').shift();
+    }
+    return undefined
   }
 
-  cleanFormData() {
-    this.formData.next([]);
-  }
-
-  setEstadoLista(estado: boolean) {
-    this.estadoLista.next(estado);
+  deleteCookie(name: string) {
+    const date = new Date();
+    // Set the expiration date in the past
+    date.setTime(date.getTime() - 1);
+    document.cookie = name + '=; expires=' + date.toUTCString() + '; path=/';
   }
 }
