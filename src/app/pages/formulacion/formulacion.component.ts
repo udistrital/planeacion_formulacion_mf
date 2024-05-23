7327cf039bfc85155e181eca7315c15c19f2ddda
Steven <st2ven.cabarcas@gmail.com>
2024-05-23 13:54:15 -0500

    feat: planes repetidos

--- src/app/pages/formulacion/formulacion.component.ts
+++ src/app/pages/formulacion/formulacion.component.ts
@@ -225,7 +225,7 @@ export class FormulacionComponent implements OnInit, OnDestroy {
         Swal.showLoading();
       }
     });
-    if (!this.existePlan(this.planesInteresArray, plan._id)) {
+    if (!this.existePlan(this.planesInteresArray, plan.nombre)) {
       this.moduloVisible = true;
       Swal.close()
     } else {
@@ -409,9 +409,15 @@ export class FormulacionComponent implements OnInit, OnDestroy {
       this.request.get(environment.PLANES_CRUD, `plan?query=activo:true,dependencia_id:${this.unidad.Id},formato:false,vigencia:${this.vigencia.Id}`).subscribe(async (data: DataRequest) => {
         if (data) {
           // No se puede traer filtrado desde PLANES_CRUD, al parecer excede la cantidad de parametros
-          this.planes = (data.Data as Plan[]).filter(
+          let planes = (data.Data as Plan[]).filter(
             (p) => p.tipo_plan_id != this.codigosService.getId(TIPO.PlanProyecto)
           );
+          this.planes = []
+          planes.forEach(plan => {
+            if (!this.existePlan(this.planes, plan.nombre)) {
+              this.planes = [...this.planes, plan]
+            }
+          });
           await this.loadPlanesPeriodoSeguimiento();
           resolve(this.planes);
         }
@@ -429,8 +435,8 @@ export class FormulacionComponent implements OnInit, OnDestroy {
     })
   }
 
-  existePlan(arreglo: (Plan | PlanInteres)[], idPlan: string): boolean {
-    return arreglo.some((plan) => plan._id === idPlan);
+  existePlan(arreglo: (Plan | PlanInteres)[], nombre: string): boolean {
+    return arreglo.some((plan) => plan.nombre === nombre);
   }
 
   async loadPlanesPeriodoSeguimiento() {
@@ -447,6 +453,7 @@ export class FormulacionComponent implements OnInit, OnDestroy {
       this.request
         .post(environment.PLANES_CRUD, `periodo-seguimiento/buscar-unidad-planes/3`, periodo_seguimiento)
         .subscribe((data: DataRequest) => {
+          this.planesInteresArray = [];
           if (data?.Data != null && data.Data.length != 0) {
             data.Data.forEach((elemento: PeriodoSeguimiento) => {
               if (elemento.planes_interes) {
@@ -455,15 +462,13 @@ export class FormulacionComponent implements OnInit, OnDestroy {
 
                   // Recorre los planes en Interes y solo agrega los que no existian
                   planesInteresArray.forEach(plan => {
-                    if (!this.existePlan(this.planesInteresArray, plan._id)) {
+                    if (!this.existePlan(this.planesInteresArray, plan.nombre)) {
                       this.planesInteresArray = [...this.planesInteresArray, plan]
-                    }
-                    if (!this.existePlan(this.planes, plan._id)) {
-                      this.planes = [...this.planes, plan]
+                      if (this.existePlan(this.planes, plan.nombre)) {
+                        this.planes = this.planes.filter((p) => p.nombre !== plan.nombre)
+                      }
                     }
                   });
-                  Swal.close();
-                  resolve(this.planes);
                 } catch (error) {
                   console.error(
                     "Error al analizar JSON en planes_interes:",
@@ -480,6 +485,7 @@ export class FormulacionComponent implements OnInit, OnDestroy {
               }
             });
           }
+          this.planes = [...this.planes, ...this.planesInteresArray];
           Swal.close();
           if (this.planes.length == 0) {
             Swal.fire({
@@ -495,6 +501,7 @@ export class FormulacionComponent implements OnInit, OnDestroy {
               timer: 7000,
             });
           }
+          resolve(this.planes);
         });
     });
   }
@@ -996,7 +1003,7 @@ export class FormulacionComponent implements OnInit, OnDestroy {
         Swal.showLoading();
       },
     })
-    if (this.existePlan(this.planesInteresArray, plan._id)) {
+    if (this.existePlan(this.planesInteresArray, plan.nombre)) {
       this.banderaEstadoDatos = true;
       Swal.close();
       return Promise.resolve();
